1a8d8478a78c5f39dd3005975ce981788c6ff0e8
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-05 09:46:59 +0500

    FlutterLocalNotificationsPlugin

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:ex_app/Var_data/database/app_db.dart';
 import 'package:ex_app/exercise_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/data/latest_all.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -29,12 +27,7 @@ void main() async {
 // Open the notification and show the reference.
   ExerciseDatabase.instance;
   AndroidAlarmManager.initialize();
-  await _configureLocalTimeZone();
+  tz.initializeTimeZones();
   runApp(const ExerciseApp());
 }
 
-Future<void> _configureLocalTimeZone() async {
-  tz.initializeTimeZones();
-  final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
-  tz.setLocalLocation(tz.getLocation(timeZoneName!));
-}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_local_notifications
-import flutter_native_timezone
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
