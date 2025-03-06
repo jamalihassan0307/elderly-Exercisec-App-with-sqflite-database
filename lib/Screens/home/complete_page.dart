e5c82e1bf62442b6b10bbdb54da6bcd528af1e50
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-06 18:53:31 +0500

    update ExerciseDetailsPage

--- lib/Screens/home/complete_page.dart
+++ lib/Screens/home/complete_page.dart
@@ -12,149 +12,232 @@ class CompletePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var duration = double.parse(arg.event.duration);
+    final duration = double.parse(arg.event.duration);
+    final time = duration > 60 ? 'Minutes' : 'Seconds';
+    final timeTo = duration > 60 ? (duration / 60).toStringAsFixed(1) : duration.toString();
 
-    var time = '';
-    var timeTo = '';
-    if (duration > 60) {
-      var min = duration / 60;
-      timeTo = min.toStringAsFixed(1);
-      time = 'Minutes';
-    } else {
-      timeTo = duration.toString();
-      time = 'Seconds';
-    }
     return Scaffold(
-      body: SafeArea(
-        child: Stack(children: [
-          Container(
-            height: height / 2.8,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(arg.levels.imagePath),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          CustomCircleButton(
-            onTap: () {
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                '/ViewAllExercisePage',
-                (route) => false,
-                arguments: arg.levels,
-              );
-            },
-            imagePath: 'back.png',
-          ),
-          SingleChildScrollView(
-            child: Container(
-              height: height,
-              width: double.infinity,
-              margin: EdgeInsets.only(top: height / 3.2),
-              padding: EdgeInsets.symmetric(
-                vertical: 2.5 * SizeConfig.height!,
-                horizontal: 2.5 * SizeConfig.height!,
-              ),
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(5 * SizeConfig.height!),
-                  topRight: Radius.circular(5 * SizeConfig.height!),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    'Well Done, You\'ve completed this workout!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 3 * SizeConfig.text!,
-                      letterSpacing: 0.7,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  h10,
-                  Image.asset('assets/icons/trophy.PNG', height: 23 * SizeConfig.height!),
-                  h10,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 3.5 * SizeConfig.height!,
-                            backgroundColor: blue,
-                            child: Text(
-                              arg.event.kcal,
-                              style: TextStyle(
-                                color: white,
-                                fontSize: 2.5 * SizeConfig.text!,
-                                letterSpacing: 0.7,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          h10,
-                          Text(
-                            'Calories Burned',
-                            style: TextStyle(
-                              color: black,
-                              fontSize: 2.2 * SizeConfig.text!,
-                              letterSpacing: 0.7,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                      w30,
-                      Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 3.5 * SizeConfig.height!,
-                            backgroundColor: blue,
-                            child: Text(
-                              timeTo,
-                              style: TextStyle(
-                                color: white,
-                                fontSize: 2.5 * SizeConfig.text!,
-                                letterSpacing: 0.7,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          h10,
-                          Text(
-                            time,
-                            style: TextStyle(
-                              color: black,
-                              fontSize: 2.2 * SizeConfig.text!,
-                              letterSpacing: 0.7,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  h30,
-                  CustomRoundBtn(
-                      onTap: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/BottomNavBar',
-                          (route) => false,
-                          arguments: ScreenArguments(0, false),
-                        );
-                      },
-                      text: 'Complete'),
-                  h10,
-                ],
-              ),
-            ),
-          )
-        ]),
+      body: Stack(
+        children: [
+          _buildHeaderImage(),
+          _buildBackButton(context),
+          _buildContentCard(context, timeTo, time),
+        ],
       ),
+    );
+  }
+
+  Widget _buildHeaderImage() {
+    return Container(
+      height: 45 * SizeConfig.height!,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(arg.levels.imagePath),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.3),
+            BlendMode.darken,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackButton(BuildContext context) {
+    return SafeArea(
+      child: CustomCircleButton(
+        onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
+          '/ViewAllExercisePage',
+          (route) => false,
+          arguments: arg.levels,
+        ),
+        imagePath: 'back.png',
+      ),
+    );
+  }
+
+  Widget _buildContentCard(BuildContext context, String timeTo, String time) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.7,
+      minChildSize: 0.6,
+      maxChildSize: 0.9,
+      builder: (context, scrollController) => Container(
+        decoration: BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(4 * SizeConfig.height!),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 20,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: SingleChildScrollView(
+          controller: scrollController,
+          child: Padding(
+            padding: EdgeInsets.all(3 * SizeConfig.height!),
+            child: Column(
+              children: [
+                _buildCongratulationsText(),
+                h20,
+                _buildTrophy(),
+                h30,
+                _buildStats(timeTo, time),
+                h30,
+                _buildCompleteButton(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCongratulationsText() {
+    return Column(
+      children: [
+        Text(
+          'Congratulations! 🎉',
+          style: TextStyle(
+            color: const Color(0xFF1A237E),
+            fontSize: 3.5 * SizeConfig.text!,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0.8,
+          ),
+        ),
+        h10,
+        Text(
+          'You\'ve completed your workout!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black87,
+            fontSize: 2.2 * SizeConfig.text!,
+            letterSpacing: 0.5,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTrophy() {
+    return TweenAnimationBuilder(
+      duration: const Duration(milliseconds: 800),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (context, double value, child) {
+        return Transform.scale(
+          scale: value,
+          child: Container(
+            padding: EdgeInsets.all(3 * SizeConfig.height!),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color(0xFFE3F2FD),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.orange.withOpacity(0.3),
+                  blurRadius: 20 * value,
+                  spreadRadius: 10 * value,
+                ),
+              ],
+            ),
+            child: child,
+          ),
+        );
+      },
+      child: Image.asset(
+        'assets/icons/trophy.PNG',
+        height: 15 * SizeConfig.height!,
+      ),
+    );
+  }
+
+  Widget _buildStats(String timeTo, String time) {
+    return Container(
+      padding: EdgeInsets.all(2 * SizeConfig.height!),
+      decoration: BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildStatItem(
+            value: arg.event.kcal,
+            label: 'Calories\nBurned',
+            icon: Icons.local_fire_department_rounded,
+            color: Colors.orange,
+          ),
+          Container(
+            width: 1,
+            height: 8 * SizeConfig.height!,
+            color: Colors.grey.withOpacity(0.2),
+          ),
+          _buildStatItem(
+            value: timeTo,
+            label: time,
+            icon: Icons.timer_rounded,
+            color: const Color(0xFF1A237E),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStatItem({
+    required String value,
+    required String label,
+    required IconData icon,
+    required Color color,
+  }) {
+    return Container(
+      padding: EdgeInsets.all(2 * SizeConfig.height!),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Icon(icon, color: color, size: 4 * SizeConfig.height!),
+          h10,
+          Text(
+            value,
+            style: TextStyle(
+              color: color,
+              fontSize: 3 * SizeConfig.text!,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          h5,
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 1.8 * SizeConfig.text!,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCompleteButton(BuildContext context) {
+    return CustomRoundBtn(
+      onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
+        '/BottomNavBar',
+        (route) => false,
+        arguments: ScreenArguments(0, false),
+      ),
+      text: 'Back to Home',
     );
   }
 }
--- lib/Screens/home/exercise_details_page.dart
+++ lib/Screens/home/exercise_details_page.dart
@@ -7,84 +7,165 @@ import 'package:flutter/material.dart';
 
 class ExerciseDetailsPage extends StatelessWidget {
   final Exercise exercise;
-  const ExerciseDetailsPage({Key? key, required this.exercise})
-      : super(key: key);
+  const ExerciseDetailsPage({Key? key, required this.exercise}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: white,
-        elevation: 0,
-        centerTitle: true,
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          _buildSliverAppBar(),
+          SliverToBoxAdapter(
+            child: _buildContent(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSliverAppBar() {
+    return SliverAppBar(
+      expandedHeight: 45 * SizeConfig.height!,
+      floating: false,
+      pinned: true,
+      stretch: true,
+      backgroundColor: const Color(0xFF1A237E),
+      leading: Builder(
+        builder: (context) => Padding(
+          padding: EdgeInsets.all(SizeConfig.height! * 0.2),
+          child: CustomCircleButton(
+            onTap: () => Navigator.pop(context),
+            imagePath: 'back.png',
+          ),
+        ),
+      ),
+      flexibleSpace: FlexibleSpaceBar(
         title: Text(
           exercise.name,
           style: TextStyle(
-            color: black.withOpacity(0.7),
-            fontSize: 3.2 * SizeConfig.text!,
+            color: white,
+            fontSize: 2.5 * SizeConfig.text!,
             fontWeight: FontWeight.bold,
           ),
         ),
-        leadingWidth: 6.5 * SizeConfig.height!,
-        leading: CustomCircleButton(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          imagePath: 'back.png',
+        background: Stack(
+          fit: StackFit.expand,
+          children: [
+            Hero(
+              tag: 'exercise_image_${exercise.name}',
+              child: Image.asset(
+                exercise.imagePath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.7),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 2 * SizeConfig.height!),
-          child: ListView(
-            children: [
-              Image.asset(
-                exercise.imagePath,
-                height: 40 * SizeConfig.height!,
-                fit: BoxFit.fill,
-              ),
-              h20,
-              for (int i = 0; exercise.steps.length > i; i++)
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 2 * SizeConfig.height!,
-                    horizontal: 1 * SizeConfig.height!,
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 2.5 * SizeConfig.height!,
-                        child: Text(
-                          '${i + 1}',
-                          style: TextStyle(
-                            color: white,
-                            fontSize: 2.6 * SizeConfig.text!,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        backgroundColor: blue,
-                      ),
-                      w20,
-                      Container(
-                        width: 35 * SizeConfig.height!,
-                        color: white,
-                        child: Text(
-                          exercise.steps[i],
-                          style: TextStyle(
-                            color: black.withOpacity(0.6),
-                            letterSpacing: 1,
-                            fontSize: 2 * SizeConfig.text!,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-            ],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(2 * SizeConfig.height!),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildSectionTitle('How to Perform'),
+          h20,
+          ...List.generate(
+            exercise.steps.length,
+            (index) => _buildStepItem(index),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 2 * SizeConfig.width!,
+        vertical: SizeConfig.height!,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xFFE3F2FD),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: const Color(0xFF1A237E),
+          fontSize: 2.4 * SizeConfig.text!,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 0.5,
         ),
+      ),
+    );
+  }
+
+  Widget _buildStepItem(int index) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 2 * SizeConfig.height!),
+      padding: EdgeInsets.all(2 * SizeConfig.height!),
+      decoration: BoxDecoration(
+        color: white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xFF1A237E).withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(1.2 * SizeConfig.height!),
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Color(0xFF303F9F), Color(0xFF1A237E)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              '${index + 1}',
+              style: TextStyle(
+                color: white,
+                fontSize: 2.2 * SizeConfig.text!,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          w20,
+          Expanded(
+            child: Text(
+              exercise.steps[index],
+              style: TextStyle(
+                color: const Color(0xFF1A237E).withOpacity(0.8),
+                fontSize: 1.8 * SizeConfig.text!,
+                height: 1.5,
+                letterSpacing: 0.5,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
